e0b15a45d218b3983a1a93be481c2a068a4edf94
Cédric Girard <cedric@daneel.net>
2020-08-09 23:23:06 +0200

    gestion tor

---
+++ bin/tour.sh
@@ -0,0 +1,54 @@
+#1
+#1
+#bin/play.sh tour/fight_gauche
+#2
+#bin/play.sh tour/fontaine/begin_droit tour/fontaine/butt1 tour/fontaine/outquestion tour/fontaine/butt2 tour/fontaine/outquestion tour/fontaine/butt3 tour/fontaine/outquestion tour/fontaine/end bin/play.sh tour/next_droit
+#3
+#bin/play.sh tour/fight_gauche
+#4
+#bin/play.sh tour/coffre/begin_droit tour/coffre/butt1 tour/coffre/end
+#5
+#bin/play.sh tour/fight_gauche
+#6
+#bin/play.sh tour/fontaine/begin_droit tour/fontaine/butt1 tour/fontaine/outquestion tour/fontaine/butt2 tour/fontaine/outquestion tour/fontaine/butt3 tour/fontaine/outquestion tour/fontaine/end bin/play.sh tour/next_droit
+#7
+#bin/play.sh tour/fight_gauche
+#8
+#bin/play.sh tour/coffre/begin_droit tour/coffre/butt1 tour/coffre/end
+#9
+#bin/play.sh tour/fight_gauche
+#10
+#bin/play.sh tour/coffre/begin_droit tour/coffre/butt1 tour/coffre/end
+#11
+#bin/play.sh tour/fight_gauche
+#12
+#bin/play.sh tour/fontaine/begin_droit tour/fontaine/butt1 tour/fontaine/outquestion tour/fontaine/butt2 tour/fontaine/outquestion tour/fontaine/butt3 tour/fontaine/outquestion tour/fontaine/end bin/play.sh tour/next_droit
+#13
+#bin/play.sh tour/fight_gauche
+#14
+#bin/play.sh tour/coffre/begin_droit tour/coffre/butt1 tour/coffre/end
+#15
+#bin/play.sh tour/fight_gauche
+#16
+#bin/play.sh tour/coffre/begin_droit tour/coffre/butt1 tour/coffre/end
+#17
+#bin/play.sh tour/fight_gauche
+#18
+#bin/play.sh tour/fontaine/begin_droit tour/fontaine/butt1 tour/fontaine/outquestion tour/fontaine/butt2 tour/fontaine/outquestion tour/fontaine/butt3 tour/fontaine/outquestion tour/fontaine/end bin/play.sh tour/next_droit
+#19
+#bin/play.sh tour/fight_gauche
+#20
+#bin/play.sh tour/coffre/begin_droit tour/coffre/butt1 tour/coffre/end
+#21
+#bin/play.sh tour/fight_gauche
+#22
+#bin/play.sh tour/coffre/begin_droit tour/coffre/butt1 tour/coffre/end
+#23
+#bin/play.sh tour/fight_gauche
+#24
+bin/play.sh tour/fontaine/begin_droit tour/fontaine/butt1 tour/fontaine/outquestion tour/fontaine/butt2 tour/fontaine/outquestion tour/fontaine/butt3 tour/fontaine/outquestion tour/fontaine/end bin/play.sh tour/next_droit
+
+
+
+
+#bin/play.sh tour/coffre/begin_droit tour/coffre/butt1 tour/coffre/end
