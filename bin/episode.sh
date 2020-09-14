9a8d27a8d1119bffd530769b55cbb9f60a999365
Cédric Girard <cedric@daneel.net>
2020-09-14 13:35:53 +0200

    ajout nb itérations max

--- bin/episode.sh
+++ bin/episode.sh
@@ -8,6 +8,6 @@ if [ -z "$3" ]; then nbmax=15; else nbmax=$3; fi
 
 bash bin/play.sh carte <(php bin/selectsaison.php $1)
 for i in $(seq 1 $nbmax); do 
-	echo "itération $i"
+	echo "itération $i/$nbmax"
 	bash bin/play.sh <(php bin/episodes.php $1 $2) continuer
 done
